c9d8441ea5f91ca809b694ad39d26c3c52bfaeb8
George Okumu <gokumu12@gmail.com>
2020-10-11 16:24:54 +0300

    Connected our main button with login activity,on clicking it directs to login form

--- app/src/main/java/com/moringa/musicchat/MainActivity.java
+++ app/src/main/java/com/moringa/musicchat/MainActivity.java
@@ -2,14 +2,29 @@ package com.moringa.musicchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private Button welcomeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        welcomeButton = (Button) findViewById(R.id.welcome);
+        welcomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
