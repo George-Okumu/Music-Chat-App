da853ebe4f760e87e6735d9302c043f8159f6192
George Okumu <gokumu12@gmail.com>
2020-10-11 18:30:39 +0300

    used toast to display a successfull message after registering

--- app/src/main/java/com/moringa/musicchat/RegisterActivity.java
+++ app/src/main/java/com/moringa/musicchat/RegisterActivity.java
@@ -3,13 +3,43 @@ package com.moringa.musicchat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText name, email, phone, password;
+    private Button register;
+    private TextView login;
+    boolean isNameValid, isEmailValid, isPhoneValid, isPasswordValid;
+    private TextInputLayout nameError, emailError, phoneError, passError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        name = (EditText) findViewById(R.id.name);
+        email = (EditText) findViewById(R.id.email);
+        phone = (EditText) findViewById(R.id.phone);
+        password = (EditText) findViewById(R.id.password);
+        login = (TextView) findViewById(R.id.login);
+        register = (Button) findViewById(R.id.register);
+        nameError = (TextInputLayout) findViewById(R.id.nameError);
+        emailError = (TextInputLayout) findViewById(R.id.emailError);
+        phoneError = (TextInputLayout) findViewById(R.id.phoneError);
+        passError = (TextInputLayout) findViewById(R.id.passError);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(RegisterActivity.this,"You have succesfully registered welcome", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 }
