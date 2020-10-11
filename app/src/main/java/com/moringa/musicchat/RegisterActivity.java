61a02eaa30d1e3b19375b90917cc2121f1bdafac
George Okumu <gokumu12@gmail.com>
2020-10-11 21:37:29 +0300

    A user can now enter login and register details

--- app/src/main/java/com/moringa/musicchat/RegisterActivity.java
+++ app/src/main/java/com/moringa/musicchat/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.moringa.musicchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,9 +39,65 @@ public class RegisterActivity extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(RegisterActivity.this,"You have succesfully registered welcome", Toast.LENGTH_LONG).show();
+               // Toast.makeText(RegisterActivity.this,"You have succesfully registered welcome", Toast.LENGTH_LONG).show();
+                SetValidation();
             }
         });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public void SetValidation() {
+        // Check for a valid name.
+        if (name.getText().toString().isEmpty()) {
+            nameError.setError(getResources().getString(R.string.name_error));
+            isNameValid = false;
+        } else {
+            isNameValid = true;
+            nameError.setErrorEnabled(false);
+        }
+
+        // Check for a valid email address.
+        if (email.getText().toString().isEmpty()) {
+            emailError.setError(getResources().getString(R.string.email_error));
+            isEmailValid = false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()) {
+            emailError.setError(getResources().getString(R.string.error_invalid_email));
+            isEmailValid = false;
+        } else {
+            isEmailValid = true;
+            emailError.setErrorEnabled(false);
+        }
+
+        // Check for a valid phone number.
+        if (phone.getText().toString().isEmpty()) {
+            phoneError.setError(getResources().getString(R.string.phone_error));
+            isPhoneValid = false;
+        } else {
+            isPhoneValid = true;
+            phoneError.setErrorEnabled(false);
+        }
+
+        // Check for a valid password.
+        if (password.getText().toString().isEmpty()) {
+            passError.setError(getResources().getString(R.string.password_error));
+            isPasswordValid = false;
+        } else if (password.getText().length() < 6) {
+            passError.setError(getResources().getString(R.string.error_invalid_password));
+            isPasswordValid = false;
+        } else {
+            isPasswordValid = true;
+            passError.setErrorEnabled(false);
+        }
+
+        if (isNameValid && isEmailValid && isPhoneValid && isPasswordValid) {
+            Toast.makeText(getApplicationContext(), "Successfully", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
