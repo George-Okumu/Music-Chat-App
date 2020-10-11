d697e3a1f5a503363a4e4b408106b9a6bb983a09
George Okumu <gokumu12@gmail.com>
2020-10-11 17:43:33 +0300

    Toast message after clicking login button

--- app/src/main/java/com/moringa/musicchat/LoginActivity.java
+++ app/src/main/java/com/moringa/musicchat/LoginActivity.java
@@ -2,13 +2,42 @@ package com.moringa.musicchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText email;
+    private EditText password;
+    TextView register;
+    private Button login;
+    boolean isEmailValid, isPasswordValid;
+    TextInputLayout emailError, passwordError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+        register = (TextView) findViewById(R.id.register);
+        emailError = (TextInputLayout) findViewById(R.id.emailError);
+        passwordError = (TextInputLayout) findViewById(R.id.passwordError);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this,"Thank you for logging in" ,Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
+
 }
