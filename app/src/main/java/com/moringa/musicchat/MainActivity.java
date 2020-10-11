f88bfecf76bbc98ab6743612be639af55c7f852e
George Okumu <gokumu12@gmail.com>
2020-10-11 21:57:14 +0300

    Added a toast to display a welcoming message

--- app/src/main/java/com/moringa/musicchat/MainActivity.java
+++ app/src/main/java/com/moringa/musicchat/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"Welcome To Music app where you can post your music and chat with your fellow musicians",Toast.LENGTH_LONG).show();
 
         welcomeButton = (Button) findViewById(R.id.welcome);
         welcomeButton.setOnClickListener(new View.OnClickListener() {
