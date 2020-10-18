b719d0c262b14719ced507aa2722642fb3bacaa6
George Okumu <gokumu12@gmail.com>
2020-10-18 11:24:12 +0300

    Confirmed if users inputted is being gathered correctly

--- app/src/main/java/com/moringa/musicchat/MusicListActivity.java
+++ app/src/main/java/com/moringa/musicchat/MusicListActivity.java
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MusicListActivity extends AppCompatActivity {
+    private TextView mMusicTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_list);
+        //gathering information from the home activity
         Intent intent = getIntent();
         String artist = intent.getStringExtra("artist");
+        mMusicTextView.setText("Here are all music of: " + artist);
     }
 }
