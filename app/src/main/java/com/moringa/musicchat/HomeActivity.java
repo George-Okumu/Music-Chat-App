6a0a8420a7a9e747359cdc467a7548a96e4576a9
George Okumu <gokumu12@gmail.com>
2020-10-18 11:00:39 +0300

    Setted the search Music button to onClick view method

--- app/src/main/java/com/moringa/musicchat/HomeActivity.java
+++ app/src/main/java/com/moringa/musicchat/HomeActivity.java
@@ -2,25 +2,44 @@ package com.moringa.musicchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
-public class HomeActivity extends AppCompatActivity {
-//    private ListView listView;
-//
-//    private String[] messages = new String[] {"At vero eos et accusamus et iusto odio dignissimos ducimus","Et harum quidem rerum facilis est et expedita distinctio.",
-//    "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms","The wise man therefore always holds in these matters to this principle of selection:",
-//    "But I must explain to you how all this mistaken idea of denouncing pleasure "," (The Extremes of Good and Evil) by Cicero, written in"};
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.searchMusicButton) Button mSearchMusicButton;
+    @BindView(R.id.searchMusicText) EditText mSearchMusicText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        ButterKnife.bind(this);
+
+        //set search button on clickListener
+        mSearchMusicButton.setOnClickListener(this);
+
 //        this.listView = (ListView) findViewById(R.id.listview);
 //        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.activity_list_item, messages);
 //        listView.setAdapter(adapter);
+
     }
 
+    @Override
+    public void onClick(View v) {
+        if(v == mSearchMusicButton){
+            String artist = mSearchMusicText.getText().toString();
+            Intent intent = new Intent(HomeActivity.this, MusicListActivity.class);
+            intent.putExtra("artist", artist);
+            startActivity(intent);
+        }
+    }
 }
--- app/src/main/java/com/moringa/musicchat/MusicClient.java
+++ app/src/main/java/com/moringa/musicchat/MusicClient.java
@@ -1,4 +1,41 @@
 package com.moringa.musicchat;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.moringa.musicchat.Constants.DEEZER_API_KEY;
+import static com.moringa.musicchat.Constants.DEEZER_BASE_URL;
+
 public class MusicClient {
+    private static Retrofit retrofit = null;
+
+    public static MusicApi getClient() {
+        if (retrofit == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request newRequest  = chain.request().newBuilder()
+                                    .addHeader("Authorization", DEEZER_API_KEY)
+                                    .build();
+                            return chain.proceed(newRequest);
+                        }
+                    })
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(DEEZER_BASE_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit.create(MusicApi.class);
+    }
 }
