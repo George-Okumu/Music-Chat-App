eda5f2f078297e8d22f3665b7ed87d3d7bb3b7bf
George Okumu <gokumu12@gmail.com>
2020-10-18 11:04:10 +0300

    Gathered users Input in MusicListActivity using getString()

--- app/src/main/java/com/moringa/musicchat/MusicListActivity.java
+++ app/src/main/java/com/moringa/musicchat/MusicListActivity.java
@@ -2,6 +2,7 @@ package com.moringa.musicchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MusicListActivity extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class MusicListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_list);
+        Intent intent = getIntent();
+        String artist = intent.getStringExtra("artist");
     }
 }
